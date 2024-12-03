c1bd43aa084ec21e221119625b8923384ebf17a0
taehoinnodep <147114106+taehoinnodep@users.noreply.github.com>
2024-12-03 10:44:15 +0900

    debug

--- src/App.tsx
+++ src/App.tsx
@@ -1,83 +1,87 @@
-import { Route, Routes, useLocation } from "react-router";
-import { routes } from "./routes";
-import Header from "./layouts/Header";
-import { columnBox } from "./styles/common.styled";
-import styled from "styled-components";
-import Contents from "./layouts/Contents";
-import Footer from "./layouts/Footer";
-import Reactions from "./layouts/Reactions";
-import Processes from "./layouts/Processes";
-import Spacing from "./components/ui/Spacing";
-import StoreLink from "./layouts/StoreLink";
-import FAB from "./layouts/FAB";
-import { useEffect, useState } from "react";
-import { GlobalPortal } from "./GlobalPortal";
-import Modal from "./components/ui/Modal";
+import { Route, Routes, useLocation } from 'react-router';
+import { routes } from './routes';
+import Header from './layouts/Header';
+import { columnBox } from './styles/common.styled';
+import styled from 'styled-components';
+import Contents from './layouts/Contents';
+import Footer from './layouts/Footer';
+import Reactions from './layouts/Reactions';
+import Processes from './layouts/Processes';
+import Spacing from './components/ui/Spacing';
+import StoreLink from './layouts/StoreLink';
+import FAB from './layouts/FAB';
+import { useEffect, useState } from 'react';
+import { GlobalPortal } from './GlobalPortal';
+import Modal from './components/ui/Modal';
 
 const App = () => {
-  const [useFullScreen, setUseFullScreen] = useState<boolean>(false);
-  const location = useLocation();
+	const [useFullScreen, setUseFullScreen] = useState<boolean>(false);
+	const location = useLocation();
 
-  useEffect(() => {
-    const currentPageRoute = routes.find((route) =>
-      location.pathname.startsWith(route.path)
-    );
-    if (currentPageRoute) document.title = currentPageRoute.title;
-    else document.title = "unknownPage";
-  }, [location.pathname]);
+	useEffect(() => {
+		const currentPageRoute = routes.find((route) => location.pathname.startsWith(route.path));
+		if (currentPageRoute) document.title = currentPageRoute.title;
+		else document.title = '위블 비즈(Wible Biz)';
+	}, [location.pathname]);
 
-  return (
-    <GlobalPortal.Provider>
-      <AppContainer $useFullScreen={useFullScreen}>
-        <Header
-          useFullScreen={useFullScreen}
-          setUseFullScreen={setUseFullScreen}
-        />
-        <Contents>
-          <Routes>
-            {routes.map((route) => (
-              <Route
-                key={route.path}
-                path={route.path}
-                element={route.element}
-              />
-            ))}
-          </Routes>
-          <Title>서비스 문의</Title>
-          <Reactions />
-          <Title>이용 프로세스 안내</Title>
-          <Processes />
-          <Spacing size={48} />
-          <StoreLink />
-        </Contents>
-        <FAB />
-        <Footer />
-      </AppContainer>
-      <Modal />
-    </GlobalPortal.Provider>
-  );
+	return (
+		<GlobalPortal.Provider>
+			<AppContainer $useFullScreen={useFullScreen}>
+				<Header
+					useFullScreen={useFullScreen}
+					setUseFullScreen={setUseFullScreen}
+				/>
+				{location.pathname === '/' ? (
+					<div></div>
+				) : (
+					<>
+						<Contents>
+							<Routes>
+								{routes.map((route) => (
+									<Route
+										key={route.path}
+										path={route.path}
+										element={route.element}
+									/>
+								))}
+							</Routes>
+							<Title>서비스 문의</Title>
+							<Reactions />
+							<Title>이용 프로세스 안내</Title>
+							<Processes />
+							<Spacing size={48} />
+							<StoreLink />
+						</Contents>
+					</>
+				)}
+				<FAB />
+				<Footer />
+			</AppContainer>
+			<Modal />
+		</GlobalPortal.Provider>
+	);
 };
 
 const AppContainer = styled.div<{ $useFullScreen: boolean }>`
-  ${columnBox};
-  width: 100%;
-  min-height: 100vh;
+	${columnBox};
+	width: 100%;
+	min-height: 100vh;
 
-  ${(props) => {
-    if (props.$useFullScreen) {
-      return `
+	${(props) => {
+		if (props.$useFullScreen) {
+			return `
         overflow: hidden;
       `;
-    }
-  }}
+		}
+	}}
 
-  height: ${(props) => (props.$useFullScreen ? "100vh" : "auto")};
+	height: ${(props) => (props.$useFullScreen ? '100vh' : 'auto')};
 `;
 
 const Title = styled.div`
-  font-size: var(--heading-2);
-  font-weight: bold;
-  margin: var(--heading-2-margin);
+	font-size: var(--heading-2);
+	font-weight: bold;
+	margin: var(--heading-2-margin);
 `;
 
 export default App;
--- src/layouts/Reactions.tsx
+++ src/layouts/Reactions.tsx
@@ -1,106 +1,115 @@
-import styled from "styled-components";
-import downloadIcon from "../assets/icons/ic_download.svg";
-import writeIcon from "../assets/icons/ic_write.svg";
-import talkIcon from "../assets/icons/ic_talk.svg";
-import { centerBox } from "../styles/common.styled";
+import styled from 'styled-components';
+import downloadIcon from '../assets/icons/ic_download.svg';
+import writeIcon from '../assets/icons/ic_write.svg';
+import talkIcon from '../assets/icons/ic_talk.svg';
+import { centerBox } from '../styles/common.styled';
 
 const Reactions = () => {
-  return (
-    <ReactionsContainer>
-      <ReactionItem>
-        <Icon src={downloadIcon} alt="Download Icon" />
-        <TextContainer>
-          <Title
-            href="https://wiblebiz.kia.com/static/media/proposal.604393960f70e45463b6.pdf"
-            download="위블비즈 상품제안서"
-          >
-            상품제안서 다운로드
-          </Title>
-        </TextContainer>
-      </ReactionItem>
-      <ReactionItem>
-        <Icon src={writeIcon} alt="Register Icon" />
-        <TextContainer>
-          <Title href="/Counsel">상담문의 등록하기</Title>
-        </TextContainer>
-      </ReactionItem>
-      <ReactionItem>
-        <Icon src={talkIcon} alt="Chat Icon" />
-        <TextContainer>
-          <Title href="https://pf.kakao.com/_xfLxjdb" target="_blank">
-            카톡으로 문의하기
-          </Title>
-          <SubText>ID: Wible Biz(위블 비즈)</SubText>
-        </TextContainer>
-      </ReactionItem>
-    </ReactionsContainer>
-  );
+	return (
+		<ReactionsContainer>
+			<ReactionItem
+				href='https://wiblebiz.kia.com/static/media/proposal.604393960f70e45463b6.pdf'
+				download='위블비즈 상품제안서'
+			>
+				<Icon
+					src={downloadIcon}
+					alt='Download Icon'
+				/>
+				<TextContainer>
+					<Title>상품제안서 다운로드</Title>
+				</TextContainer>
+			</ReactionItem>
+			<ReactionItem href='/Counsel'>
+				<Icon
+					src={writeIcon}
+					alt='Register Icon'
+				/>
+				<TextContainer>
+					<Title>상담문의 등록하기</Title>
+				</TextContainer>
+			</ReactionItem>
+			<ReactionItem
+				href='https://pf.kakao.com/_xfLxjdb'
+				target='_blank'
+			>
+				<Icon
+					src={talkIcon}
+					alt='Chat Icon'
+				/>
+				<TextContainer>
+					<Title>카톡으로 문의하기</Title>
+					<SubText>ID: Wible Biz(위블 비즈)</SubText>
+				</TextContainer>
+			</ReactionItem>
+		</ReactionsContainer>
+	);
 };
 
 const ReactionsContainer = styled.div`
-  display: flex;
-  gap: 1rem;
-  flex-wrap: wrap;
-  justify-content: center;
+	display: flex;
+	gap: 1rem;
+	flex-wrap: wrap;
+	justify-content: center;
 
-  @media (max-width: 1024px) {
-    justify-content: space-between;
-  }
+	@media (max-width: 1024px) {
+		justify-content: space-between;
+	}
 
-  @media (max-width: 744px) {
-    flex-direction: column;
-    align-items: center;
-  }
+	@media (max-width: 744px) {
+		flex-direction: column;
+		align-items: center;
+	}
 `;
 
-const ReactionItem = styled.div<{ $highlighted?: boolean }>`
-  ${centerBox}
-  flex: 1 1 calc(33.333% - 1rem);
-  width: 100%;
-  border: 1px solid var(--midnight-900);
-  padding: 1rem;
-  cursor: pointer;
+const ReactionItem = styled.a`
+	${centerBox}
+	flex: 1 1 calc(33.333% - 1rem);
+	width: 100%;
+	border: 1px solid var(--midnight-900);
+	padding: 1rem;
+	cursor: pointer;
+	color: var(--midnight-900);
+	text-decoration: none;
 
-  &:hover {
-    background-color: var(--gray-50);
-  }
+	&:hover {
+		background-color: var(--gray-50);
+	}
 
-  @media (max-width: 1024px) {
-    flex: 1 1 calc(50% - 1rem);
-  }
+	@media (max-width: 1024px) {
+		flex: 1 1 calc(50% - 1rem);
+	}
 
-  @media (max-width: 744px) {
-    flex: 1 1 100%;
-    max-width: 100%;
-    justify-content: flex-start;
-    padding: 14px;
-  }
+	@media (max-width: 744px) {
+		flex: 1 1 100%;
+		max-width: 100%;
+		justify-content: flex-start;
+		padding: 14px;
+	}
 `;
 
 const Icon = styled.img`
-  width: var(--ic-lg);
-  height: var(--ic-lg);
-  margin-right: 1rem;
+	width: var(--ic-lg);
+	height: var(--ic-lg);
+	margin-right: 1rem;
 `;
 
 const TextContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
 `;
 
-const Title = styled.a`
-  font-size: 1rem;
-  font-weight: bold;
-  color: var(--midnight-900);
-  color: inherit;
-  text-decoration: none;
+const Title = styled.span`
+	font-size: 1rem;
+	font-weight: bold;
+	color: var(--midnight-900);
+	text-decoration: none;
 `;
 
 const SubText = styled.div`
-  font-size: 0.875rem;
-  color: var(--gray-500);
-  margin-top: 0.5rem;
+	font-size: 0.875rem;
+	color: var(--gray-500);
+	margin-top: 0.5rem;
 `;
 
 export default Reactions;
